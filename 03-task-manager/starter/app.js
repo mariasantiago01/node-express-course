6295f0a21bdec51766068a7b6fd211abfa4779c8
mariasantiago01 <96029417+mariasantiago01@users.noreply.github.com>
2022-11-29 23:42:45 -0500

    Update app.js
    converted console.log listen statement into a function

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -19,7 +19,7 @@ const port = 3000
 const start = async () => {
     try {
         await connectDB(process.env.MONGO_URI)
-        app.listen(port, console.log(`Server is listening on port ${port}...`))
+        app.listen(port, function() {console.log(`Server is listening on port ${port}...`)})
 
     } catch (error) {
         console.log(error)
